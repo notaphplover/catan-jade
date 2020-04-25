1002b9be27f96d444f546d9abd0469bcbef736a8
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-04-25 15:05:02 +0200

    Add command serialization

---
+++ src/io/notaphplover/catan/jade/serialization/command/CommandSerializer.java
@@ -0,0 +1,9 @@
+package io.notaphplover.catan.jade.serialization.command;
+
+import io.github.notaphplover.catan.core.command.ICommand;
+
+public class CommandSerializer extends BaseCommandSerializer<ICommand> {
+
+  private static final long serialVersionUID = -2121903246536401475L;
+
+}
